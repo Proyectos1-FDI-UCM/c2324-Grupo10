3418ff3f529b2d4c479f76b8e4b3431dc202cbed
Juan <juasan15@ucm.es>
2024-02-20 16:52:11 +0100

    Avanzando

--- Blunderbuss/Assets/Scripts/ShotManager.cs
+++ Blunderbuss/Assets/Scripts/ShotManager.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class ShotManager : MonoBehaviour
 {
-    
+    #region references
+    private PlayerManager _playerManager;
+    public Transform _myTransform;
+    public GameObject _escopetaColliderAire;
+    public GameObject _escopetaColliderSuperficie;
+
+    #endregion
+
+    #region parameters
+    #endregion
+
+    void Start()
+    {
+        _escopetaColliderAire.SetActive(false);
+        _escopetaColliderSuperficie.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (_playerManager.state == 2 || (_playerManager.state == 4 && dir == 1) || (_playerManager.state == 0 && dir == 1))
+        {
+            _escopetaColliderSuperficie.SetActive(true);
+        }
+        else
+        {
+            _escopetaColliderAire.SetActive(true);
+        }
+    }
 }
