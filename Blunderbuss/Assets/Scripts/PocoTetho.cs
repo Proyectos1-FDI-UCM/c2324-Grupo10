b163cf5dc3e50e1e5add47af8595ca7c813500fe
DaniRuizG <daniruizgopegui@gmail.com>
2024-03-07 22:51:58 +0100

    Pocotetho
    Ayuda no puedo mas

---
+++ Blunderbuss/Assets/Scripts/PocoTetho.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourCript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
