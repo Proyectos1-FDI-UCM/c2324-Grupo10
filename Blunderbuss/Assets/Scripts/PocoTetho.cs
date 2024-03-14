a1725c7d93d566ac9d88f38e279483d61da67821
DaniRuizG <daniruizgopegui@gmail.com>
2024-03-14 20:54:02 +0100

    Tutorial Terminado
    caguen dio

--- Blunderbuss/Assets/Scripts/PocoTetho.cs
+++ Blunderbuss/Assets/Scripts/PocoTetho.cs
@@ -1,16 +1,36 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NewBehaviourCript : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    #region parameters
+    [SerializeField] 
+    private GameObject _Canvas;
+    private float tiempoActivo = 5f;
+    #endregion
 
+    #region methods
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _Canvas.SetActive(true);
+        StartCoroutine(mantente());
+       
+    }
+    public IEnumerator mantente()
+    {
+        yield return new WaitForSeconds(tiempoActivo);
+        _Canvas.SetActive(false);
+    }
+    #endregion
+
+    private void Start()
+    {
+       _Canvas.SetActive(false );
+    }
     void Update()
     {
         
     }
-
 }
