96336057602d212fe4a5bb50473ae7350bac0770
DaniRuizG <daniruizgopegui@gmail.com>
2024-04-06 16:51:15 +0200

    HealthManager para bosses y barra de vida generalizable
    lo mejor del deporte

--- Blunderbuss/Assets/Scripts/BossHealth.cs
+++ Blunderbuss/Assets/Scripts/BossHealth.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class BossHealth : MonoBehaviour
 {
     #region references
     private SpriteRenderer _spriteBoss;
+    [SerializeField]
+    GameObject HealthBar;
+    [SerializeField]
+    GameObject Boss;
+    private Transform _HealthBarTra;
     #endregion
 
     #region parameters
+    public bool debug;
     public float maxHealth;
     public float health;
+    public float DamageAmmount;
     #endregion
 
     #region methods
@@ -25,16 +33,39 @@ public class BossHealth : MonoBehaviour
     private IEnumerator Damage()
     {
         _spriteBoss.color =  new Color(1, 0.1f, 0, 1);
+        DamageAmmount = 100f;
+        ActualizaVida();
 
         yield return new WaitForSeconds(0.3f);
 
         _spriteBoss.color = new Color(1, 1, 1, 1);
     }
+
+    private void ActualizaVida()
+    {
+        health -= DamageAmmount; 
+        _HealthBarTra.localScale = new Vector3(6.5122f*(health / maxHealth) , 0.27501f, 1);
+        if (health <= 0)
+        {
+            Muere();
+        }
+    }
+
+    private void Muere()
+    {
+         Boss.SetActive(false);
+    }
     #endregion
-    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         _spriteBoss = GetComponent<SpriteRenderer>();
+        health = maxHealth;
+        _HealthBarTra = HealthBar.transform;
     }
 
     // Update is called once per frame
@@ -42,4 +73,13 @@ public class BossHealth : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        if (debug)
+        {
+            print("Vida actual " + health);
+        }
+    }
+
 }
